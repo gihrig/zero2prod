cbee513ecd093db795e56a4bc248bff0b2f8974c
gihrig <glen@arkadias.com>
2020-11-15 01:13:12 -0800

    test(health_check): 4.5.2. Choosing A Random Port

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use chapter03_0::run;
+use std::net::TcpListener;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    run(address)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,21 +1,30 @@
+use chapter03_0::run;
+use std::net::TcpListener;
 
-// `actix_rt::test` is the testing equivalent of `actix_rt::main`.
-// It also spares you from having to specify the `#[test]` attribute.
+// Launch our application in the background
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
+}
+
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name of the test file)
 #[actix_rt::test]
 async fn health_check_works() {
-    // No .await, no .expect
-    spawn_app();
-    // We brought `reqwest` in as a _development_ dependency
-    // to perform HTTP requests against our application.
-    // Either add it manually under [dev-dependencies] in Cargo.toml
-    // or run `cargo add reqwest --dev`
+    // Arrange
+    let address = spawn_app();
+
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health-check")
+        // Use the returned application address
+        .get(&format!("{}/health-check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -23,19 +32,4 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-// Launch our application in the background
-// No .await call, therefore no need for `spawn_app` to be async now.
-// We are also running tests, so it is not worth it to propagate errors:
-// if we fail to perform the required setup we can just panic and crash
-// all the things.
-fn spawn_app() {
-    // New dev dependency - let's add tokio to the party with
-    // `cargo add tokio --dev --vers 0.2.22`
-    let server = chapter03_0::run().expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    let _ = tokio::spawn(server);
 }
