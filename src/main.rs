99222fd3d09733f62dc22358b1a078a41aa9e4db
gihrig <glen@arkadias.com>
2020-12-12 09:42:52 -0800

    feature(src/configuration): create host-specific configuration feature

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 //! src/main.rs
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
+    // panic if we can't read the configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run(address)?.await
+    run(listener)?.await
 }
