9f8078d7603e8c8b42d775761a6fd5eb7a9f76bd
gihrig <glen@arkadias.com>
2021-05-15 17:13:11 -0700

    feat(app): update chapter 05 from upstream

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-//! src/main.rs
-use sqlx::postgres::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -7,25 +6,21 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // panic if we can't read the configuration
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-    
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_with(configuration.database.with_db())
         .await
         .expect("Failed to connect to Postgres.");
 
-    // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
-    // reasons. This binding may cause issues in some environments. For example,
-    // it causes connectivity issues running in WSL2, where you cannot reach the
-    // server when it is bound to WSL2's localhost interface. As a workaround,
-    // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
-    // of the security implications when you expose the server on all interfaces.
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
     run(listener, connection_pool)?.await?;
     Ok(())
 }
